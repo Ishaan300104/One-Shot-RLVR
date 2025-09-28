53dfc9a3e0c3c3ac6c94b03f4455ab15e8564cf3
Ishaan300104 <ishaan300104@gmail.com>
2025-09-28 11:50:29 +0530

    klklkl

--- Qwen2.5-Eval/evaluation/sh/eval_all_math.sh
+++ Qwen2.5-Eval/evaluation/sh/eval_all_math.sh
@@ -32,8 +32,7 @@ if [ "$ALL_EXIST" = true ]; then
 else
     TOKENIZERS_PARALLELISM=false \
     python -u math_eval.py \
-        # --model_name_or_path ${MODEL_NAME_OR_PATH} \
-        --model_name_or_path "C:/Users/uvisy/Desktop/FMGA_minor/One-Shot-RLVR/verl_few_shot/Qwen2.5-Math-1.5B-pi1_r128/global_step_20/actor" \
+        --model_name_or_path ${MODEL_NAME_OR_PATH} \
         --data_name ${DATA_NAMES} \
         --output_dir ${OUTPUT_DIR} \
         --split ${SPLIT} \
